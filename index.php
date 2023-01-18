05351aab00e4e8346cb842fc1c14b8d6e70577e5
lukachochua <lukachochua@gmail.com>
2023-01-19 00:07:16 +0400

    Episode 5 - booleans and conditionals

--- index.php
+++ index.php
@@ -2,17 +2,31 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-
-    <title>Document</title>
+    <title>Demo</title>
+    <style>
+        body {
+            display: grid;
+            place-items: center;
+            height: 100vh;
+            margin: 0;
+            font-family: sans-serif;
+        }
+    </style>
 </head>
 <body>
-    <h1>
-        <?php 
-            $greeting = 'Hello';
-        
+    <?php
+        $name = "Dark Matter";
+        $read = true;
 
-            echo "$greeting, Everybody";
-        ?>
+
+        if($read) {
+            $message = "You have read the \"$name\"";
+        } else {
+            $message = "You have not read the \"$name\"";
+        }
+    ?>
+    <h1>
+        <?= $message ?>
     </h1>
 </body>
 </html>
