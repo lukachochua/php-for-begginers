dfeb99636cad4d2ef65dfb726f47b9cbe6769fba
lukachochua <lukachochua@gmail.com>
2023-01-18 23:33:40 +0400

    Episodes 1-4: PHP tags and variables

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+
+    <title>Document</title>
+</head>
+<body>
+    <h1>
+        <?php 
+            $greeting = 'Hello';
+        
+
+            echo "$greeting, Everybody";
+        ?>
+    </h1>
+</body>
+</html>
