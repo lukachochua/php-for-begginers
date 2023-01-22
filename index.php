d38e092fb60ae58f97c22aa64d7b8391c6d50e9e
lukachochua <lukachochua@gmail.com>
2023-01-23 00:14:14 +0400

    Episode 8: Functions and Filters

--- index.php
+++ index.php
@@ -10,21 +10,48 @@
             [
                 'name'=>'Do Androids Dream of Electric Sheep',
                 'author'=>'Philip K. Dick',
+                'releaseYear' => 1968,
                 'purchaseUrl'=>'http://example.com'
             ],
-
             [
                 'name'=>'Project Hail Mary',
                 'author'=>'Andy Weir',
+                'releaseYear' => 2021,
+                'purchaseUrl'=>'http://example.com'
+            ],
+            [
+                'name'=>'The Martian',
+                'author'=>'Andy Weir',
+                'releaseYear' => 2011,
                 'purchaseUrl'=>'http://example.com'
             ],
         ];
+
+        function filterByAuthor($books, $author) {
+            $filteredBooks = [];
+
+            foreach ($books as $book) {
+                if($book['author'] === $author) {
+                    $filteredBooks[] = $book;
+                }
+
+            }
+            return $filteredBooks;
+        }
+
+        
     ?>
     
-    <?php foreach($books as $book) : ?>
-        <a href="<?=$book['purchaseUrl']; ?>">
-            <li><?= $book['name']; ?></li>
-        </a>
-    <?php endforeach ?>
+    <ul>
+        <?php foreach(filterByAuthor($books, 'Philip K. Dick') as $book) : ?>
+                <li>
+                    <a href="<?=$book['purchaseUrl']; ?>">
+                        <?= $book['name'] ?> (<?= $book['releaseYear'] ?>) - by <?= $book['author'] ?> 
+                    </a>
+                </li>
+        <?php endforeach ?>
+    </ul>
+
+    
 </body>
 </html> 
