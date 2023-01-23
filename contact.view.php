1f73de18d9584cc184fc6e8f4d2432402fd502b5
lukachochua <lukachochua@gmail.com>
2023-01-23 19:35:07 +0400

    Episode 12: page links

--- contact.view.php
+++ contact.view.php
@@ -19,9 +19,9 @@
                         <div class="hidden md:block">
                             <div class="ml-10 flex items-baseline space-x-4">
                                 <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                                <a href="/" class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">Home</a>
+                                <a href="/" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">Home</a>
                                 <a href="/about.php" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">About</a>
-                                <a href="/contact.php" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Contact</a>
+                                <a href="/contact.php" class="bg-gray-900 text-white hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Contact</a>
                             </div>
                         </div>
                     </div>
@@ -134,14 +134,14 @@
 
         <header class="bg-white shadow">
             <div class="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
-                <h1 class="text-3xl font-bold tracking-tight text-gray-900">Home Page</h1>
+                <h1 class="text-3xl font-bold tracking-tight text-gray-900">Contact Us</h1>
             </div>
         </header>
         <main>
             <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
                 <!-- Replace with your content -->
                 <div class="px-4 py-6 sm:px-0">
-                    <div class="h-96 rounded-lg">Hello, Welcome to the home page</div>
+                    <div class="h-96 rounded-lg">Hello, Welcome to the contact page</div>
                 </div>
                 <!-- /End replace -->
             </div>
