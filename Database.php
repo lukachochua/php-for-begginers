b01c441354a781b2e36a66b2cdb81e4a14ed2330
lukachochua <lukachochua@gmail.com>
2023-01-29 19:14:52 +0400

    Episode 25: programming is rewriting

--- Database.php
+++ Database.php
@@ -3,6 +3,7 @@
 class Database 
 {
     public $connection;
+    public $statement;
 
     public function __construct($config, $username = 'root', $password='')
     {
@@ -14,12 +15,33 @@ class Database
     }
 
     public function query($query, $params=[]) {
-        $statement = $this->connection->prepare($query);
+        $this->statement = $this->connection->prepare($query);
         
-        $statement->execute($params);
+        $this->statement->execute($params);
        
-        return $statement;
-
+        return $this;
     }
 
+    public function get()
+    {
+        return $this->statement->fetchAll();
+    }
+
+    public function find()
+    {
+        return $this->statement->fetch();
+    }
+
+    public function FindOrFail()
+    {
+        $result = $this->find();
+
+        if(! $result) {
+            abort();
+        }
+
+        return $result;
+    }
+
+    
 }
--- controllers/note.php
+++ controllers/note.php
@@ -8,18 +8,14 @@ $db = new Database($config["database"]);
 $heading = 'Note';
 
 $currentUserId = 3;
-$currentUserId = 3;
 
 $note = $db->query('select * from notes where id=:id', [
     'id' => $_GET['id']
-])->fetch();
+])->find();
 
-if (! $note) {
-    abort();
-}
+authorize($note['user_id'] === $currentUserId);
 
-if($note['user_id'] !== $currentUserId) {
-    abort(Response::FORBIDDEN);
-}
+
+
 
 require 'views/note.view.php';
