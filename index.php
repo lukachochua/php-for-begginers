1595e6d0ad699fff9165177a798a5c70ca03231b
lukachochua <lukachochua@gmail.com>
2023-01-29 18:19:28 +0400

    Episode 23: introduction to authorization

--- index.php
+++ index.php
@@ -2,7 +2,9 @@
 
 require 'functions.php';
 require 'Database.php';
+require 'Response.php';
 require 'router.php';
+
 
 
 
---
+++ views/403.php
@@ -0,0 +1,20 @@
+<?php require 'partials/head.php' ?>
+
+<?php require 'partials/nav.php' ?>
+
+
+<main>
+    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+        <!-- Replace with your content -->
+        <div class="px-4 py-6 sm:px-0">
+            <h1 class="text-2xl font-bold">You are not authorized to view this page</h1>
+            <p class="mt-4">
+                <a class="text-blue-700 underline" href="/">Go back home.</a>
+            </p>
+
+        </div>
+        <!-- /End replace -->
+    </div>
+</main>
+
+<?php require 'partials/footer.php' ?>
