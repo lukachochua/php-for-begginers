b8da81aa9c005c8a487164d9ceaf91b8f5227e84
lukachochua <lukachochua@gmail.com>
2024-12-13 11:58:27 +0400

    feat: log in and log out

--- Core/functions.php
+++ Core/functions.php
@@ -16,7 +16,8 @@ function urlIs($value)
     return $_SERVER['REQUEST_URI'] === $value;
 }
 
-function abort($code = 404) {
+function abort($code = 404)
+{
     http_response_code($code);
 
     require base_path("views/{$code}.php");
@@ -41,4 +42,21 @@ function view($path, $attributes = [])
     extract($attributes);
 
     require base_path('views/' . $path);
+}
+
+function login($user)
+{
+    $_SESSION['user'] = [
+        'email' => $user['email']
+    ];
+
+    session_regenerate_id(true);
+}
+
+function logout()
+{
+    $_SESSION = [];
+    session_destroy();
+    $params = session_get_cookie_params();
+    setcookie('PHPSESSID', '', time() - 3600, $params['path'], $params['domain'], $params['secure'], $params['httponly']);
 }
